4dac41637176795784f771e5fc0426538f485194
Ismail424 <cicas03@hotmail.com>
2024-03-21 19:45:26 +0100

    Fixed Error Page

--- src/routes/(app)/screen/+error.svelte
+++ src/routes/(app)/screen/+error.svelte
@@ -2,4 +2,39 @@
     import { page } from '$app/stores';
 </script>
 
-<h1>{$page.status}: {$page.error?.message}</h1>
+<div class="error-container">
+    <h2>Error {$page.status}</h2>
+    <p>{$page.error?.message}</p>
+</div>
+
+
+<style lang="scss">
+    .error-container {
+        margin: 1rem 2rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 2rem;
+        text-align: center;
+        background-color: #ffeded; // light red background for error
+        border: 1px solid #e0a7a7; // subtle border
+        border-radius: 10px;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1); // shadow for depth
+
+        h2 {
+            font-family: 'Poppins', sans-serif;
+            font-weight: 700; // bold font for error status
+            color: #d43f00; // dark red color for emphasis
+            margin-bottom: 0.5rem; // spacing between the status and the message
+            font-size:  2rem;
+            text-transform: uppercase;
+        }
+
+        p {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-size: 1.3rem; // slightly larger font for the error message
+            color: #333; // dark color for readability
+        }
+    }
+</style>
--- src/routes/(app)/screen/+page.svelte
+++ src/routes/(app)/screen/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import type { PageData } from './$types';
-    
     export let data: PageData;
 </script>
+
+<p>Fajr 00:00</p>
+<p>Dhuhr 00:00</p>
+<p>Asr 00:00</p>
+<p>Maghrib 00:00</p>
+<p>Isha 00:00</p>
