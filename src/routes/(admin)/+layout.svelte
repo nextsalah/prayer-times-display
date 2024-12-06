8a60bd252d45e6ef234a11203fcb1e3340809a96
ismail424 <cicas03@hotmail.com>
2024-12-06 11:23:58 +0100

    Added Navigationbar

--- src/routes/(admin)/+layout.svelte
+++ src/routes/(admin)/+layout.svelte
@@ -3,11 +3,16 @@
   import HeadWrapper from '$lib/components/HeadWrapper.svelte';
   import BottomNavigation from '$lib/components/general/BottomNavigation.svelte';
   import { page } from '$app/stores';
+    import Navbar from '$lib/components/general/Navbar.svelte';
 
   $: isControlPanel = $page.url.pathname === '/controlpanel';
 </script>
 
+<!-- HeadWrapper is a component that sets the page title and meta tags. -->
 <HeadWrapper page_title={$page.data.title || ""} />
+
+<!-- Body -->
+<Navbar />
 <slot />
 {#if !isControlPanel}
   <BottomNavigation />
--- src/routes/(admin)/controlpanel/+page.svelte
+++ src/routes/(admin)/controlpanel/+page.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
     import { Motion } from "svelte-motion";
-    import { Card } from "flowbite-svelte";
-    import type { PageData } from './$types';
     import { navigation } from '$lib/config/navigation';
-
 </script>
   
 <div class="container mx-auto p-6">
-<h1 class="text-4xl font-bold mb-8 text-center">Control Panel</h1>
-
-<div class="grid grid-cols-1 gap-6 justify-items-center sm:grid-cols-2 lg:grid-cols-3">
-    {#each navigation.items as item}
-    <Motion let:motion whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-        <Card href="/{item.slug}" class="flex flex-col items-center text-center w-full max-w-sm p-6" use={motion}>
-        <img src={item.icon} alt={item.title} class="h-16 w-16 mb-4">
-        <h2 class="text-2xl font-semibold mb-2">{item.title}</h2>
-        <p class="text-gray-600">{item.description}</p>
-        </Card>
-    </Motion>
-    {/each}
-</div>
+    <div class="grid grid-cols-1 gap-6 justify-items-center sm:grid-cols-2 lg:grid-cols-3">
+        {#each navigation.items as item}
+        <Motion let:motion whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+            <a href="/{item.slug}" class="card bg-base-100 shadow-xl w-full max-w-sm" use:motion>
+            <figure class="px-10 pt-10">
+                <img src={item.icon} alt={item.title} class="h-16 w-16 mb-4 rounded-full">
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title text-2xl mb-2">{item.title}</h2>
+                <p class="text-gray-600">{item.description}</p>
+            </div>
+            </a>
+        </Motion>
+            {/each}
+    </div>
 </div>
 
